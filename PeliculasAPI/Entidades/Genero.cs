9f1afa0db93f49c1642daaabc8cf06029196d479
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2024-10-14 09:11:11 -0600

    Fix message of error with field Nombre in gender model when is applied the maxlength validation.

--- PeliculasAPI/Entidades/Genero.cs
+++ PeliculasAPI/Entidades/Genero.cs
@@ -8,6 +8,8 @@ namespace APP_PELICULAS.Entities
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(50,ErrorMessage ="El campo {0} debe ser menor a 50 caracteres.")]
+        [StringLength(50)]
         [FirstCapitalLetter]
         public required string Nombre { get; set; }
     }
