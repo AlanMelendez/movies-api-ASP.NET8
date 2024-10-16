739d43c8eb44d9c09a6af5ec22d20d9e8766c8d1
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2024-10-16 08:30:53 -0600

    Creating PaginacionDTO for the pagination control of frontend

---
+++ PeliculasAPI/DTOs/PaginacionDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace PeliculasAPI.DTOs
+{
+    public class PaginacionDTO
+    {
+        public int _pagina { get; set; } = 1;
+        private int _recordsPorPagina = 10;
+        private readonly int _maxRecordsPorPagina = 50;
+        public int RecordsPorPagina
+        {
+            get
+            {
+                return _recordsPorPagina;
+            }
+            set
+            {
+                _recordsPorPagina = (value > _maxRecordsPorPagina) ? _maxRecordsPorPagina : value;
+            }
+        }
+
+    }
+}
